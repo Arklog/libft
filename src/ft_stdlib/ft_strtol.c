d63c8766d91a68a9be540d82fff8751aec3c02f0
Arklog <pierr.ducloux@gmail.com>
2023-05-07 20:56:47 +0200

    ft_strtol.c

--- src/ft_stdlib/ft_strtol.c
+++ src/ft_stdlib/ft_strtol.c
@@ -6,11 +6,14 @@
 /*   By: pierre <pierre@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 20:22:26 by pierre            #+#    #+#             */
-/*   Updated: 2023/05/07 20:29:10 by pierre           ###   ########.fr       */
+/*   Updated: 2023/05/07 20:56:15 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdlib.h"
+#include "ft_string.h"
+#include "ft_math.h"
+#include "libft.h"
 
 static const char	*g_base_8 = "01234567";
 static const char	*g_base_10 = "0123456789";
@@ -19,7 +22,11 @@ static const char	*g_base_16 = "0123456789abcdef";
 long	ft_strtol(const char *str, char **endptr, int base)
 {
 	char	*_base;
+	long	result;
+	int		negative;
+	int		i;
 
+	*endptr = (char *)str;
 	if (base == 8)
 		_base = g_base_8;
 	else if (base == 10)
@@ -28,4 +35,15 @@ long	ft_strtol(const char *str, char **endptr, int base)
 		_base = g_base_16;
 	else
 		return (0);
+	negative = 0;
+	if (str[0] == '-')
+		negative = *(str++);
+	result = 0;
+	i = 0;
+	while (ft_strlen(str))
+	{
+		result += (ft_strchr(_base, *(str++)) - _base) * ft_powi(base, i++);
+		++(*endptr);
+	}
+	return ((negative * -1) * result);
 }

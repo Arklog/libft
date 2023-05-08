3114add4876485b196206ec1b4fd4086d7831fba
Arklog <pierr.ducloux@gmail.com>
2023-05-08 17:13:55 +0200

    test

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: pierre <pierre@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 16:54:39 by pierre            #+#    #+#             */
-/*   Updated: 2023/05/07 20:59:18 by pierre           ###   ########.fr       */
+/*   Updated: 2023/05/08 17:11:28 by pierre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include <sys/types.h>
 #include <stdio.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char		a[128] = {0};
 	u_int32_t	b;
 	int 		bw;
 
+	(void)argc;
+	(void)argv;
 	ft_memset(a, 'a', 127);
 	a[0] = 0;
 	b = *((u_int32_t *)a);

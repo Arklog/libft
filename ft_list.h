a4b31c1ec7e84caf6dd3db42312d16c79a47bfb1
Pierre DUCLOUX <pducloux@paul-f4Br4s6.clusters.42paris.fr>
2023-06-14 16:47:12 +0200

    ft_list.h

---
+++ ft_list.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pducloux <pducloux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 12:58:34 by pducloux          #+#    #+#             */
+/*   Updated: 2023/06/14 12:58:54 by pducloux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct s_list {
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+
+void	ft_lstaddfront(t_list **lst, t_list *new);
+
+void	ft_lstaddback(t_list **lst, t_list *new);
+
+int		ft_lstsize(t_list	*lst);
+
+t_list	*ft_lstlast(t_list	*lst);
+
+int		ft_lstiteri(t_list *lst, int (*f)(size_t i, void *content));
+#endif

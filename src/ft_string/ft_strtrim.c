8f6dbd05565f4960af39155f1ccc86035dcb9add
Pierre DUCLOUX <pducloux@made-f0Br8s16.clusters.42paris.fr>
2023-12-09 13:40:12 +0100

    update

--- src/ft_string/ft_strtrim.c
+++ src/ft_string/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vtestut <vtestut@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pducloux <pducloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:23:11 by pierre            #+#    #+#             */
-/*   Updated: 2023/12/06 16:30:30 by vtestut          ###   ########.fr       */
+/*   Updated: 2023/12/09 13:38:57 by pducloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,17 @@ int	ft_strtrim(char *str, const char *set)
 	iter = str;
 	while (*iter && ft_strchr(set, *iter))
 		iter++;
-//	if (!(*iter))
-//		return ((int)(iter - str));
 	n = ft_strlen(iter);
 	removed = iter - str;
 	ft_memmove(str, iter, n + 1);
-	iter = iter + n - 1;
+	if (!n)
+		return (removed);
+	iter = str + ft_strlen(str) - 1;
 	while (*iter && ft_strchr(set, *iter))
+	{
+		*iter = '\0';
 		iter--;
+	}
 	removed += ft_strlen(iter);
 	ft_memset(iter + 1, '\0', ft_strlen(iter + 1));
 	return (removed);
